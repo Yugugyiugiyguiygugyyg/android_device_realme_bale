d7ec9c4af4c57118c5a2463abe9cd403f0e0ae58
A4 <101423101+Yugugyiugiyguiygugyyg@users.noreply.github.com>
2026-04-10 15:29:18 +0300

    Update lineage_bale_GAPPS.mk

--- lineage_bale_GAPPS.mk
+++ lineage_bale_GAPPS.mk
@@ -14,7 +14,7 @@ $(call inherit-product, device/realme/bale/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_bale
+PRODUCT_NAME := lineage_bale_GAPPS
 PRODUCT_DEVICE := bale
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
