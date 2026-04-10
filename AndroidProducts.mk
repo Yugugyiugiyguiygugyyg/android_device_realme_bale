74d265d8c6841d5dec01310d09653b34769d6d58
A4 <101423101+Yugugyiugiyguiygugyyg@users.noreply.github.com>
2026-04-10 15:37:43 +0300

    Update AndroidProducts.mk
    #
    # Copyright (C) 2025 The LineageOS Project
    #
    # SPDX-License-Identifier: Apache-2.0
    #
    
    PRODUCT_MAKEFILES := \
        $(LOCAL_DIR)/lineage_bale.mk \
        $(LOCAL_DIR)/lineage_bale_GAPPS.mk
    
    COMMON_LUNCH_CHOICES := \
        lineage_bale-user \
        lineage_bale-userdebug \
        lineage_bale-eng \
        lineage_bale_GAPPS-user \
        lineage_bale_GAPPS-userdebug \
        lineage_bale_GAPPS-eng

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_bale.mk
+    $(LOCAL_DIR)/lineage_bale.mk \
+    $(LOCAL_DIR)/lineage_bale_GAPPS.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_bale-user \
+    lineage_bale-userdebug \
+    lineage_bale-eng \
+    lineage_bale_GAPPS-user \
+    lineage_bale_GAPPS-userdebug \
+    lineage_bale_GAPPS-eng
